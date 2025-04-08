d2a893cc222da9efabb5cac5040bb4c13a371dc7
Maltesh B K <malteshbk1999@gmail.com>
2025-04-08 16:45:22 +0530

    added react helmet for seo

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-
-import HowToUseSection from './static/HowToUseSection'
-import WhyToUseSection from './static/WhyToUseSection'
-import DataTableSection from './static/DataTableSection'
-import Footer from './components/Footer'
+import HowToUseSection from "./static/HowToUseSection";
+import WhyToUseSection from "./static/WhyToUseSection";
+import DataTableSection from "./static/DataTableSection";
+import Footer from "./components/Footer";
 // import { Toaster } from './components/ui/sonner'
-import { printConsole } from './utils/utils'
-import { Toaster } from 'react-hot-toast'
-import Header from './components/Header'
-import Hero from './static/Hero'
+import { printConsole } from "./utils/utils";
+import { Toaster } from "react-hot-toast";
+import Header from "./components/Header";
+import Hero from "./static/Hero";
+import { Helmet } from "react-helmet";
 
 function App() {
-
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [videoData, setVideoData] = useState(null);
   const [audioFormats, setAudioFormats] = useState([]);
   const [videoFormats, setVideoFormats] = useState([]);
@@ -27,7 +26,7 @@ function App() {
   const [open, setOpen] = useState(false);
 
   const updateVideoData = (videoData) => {
-    console.log("on APP Update Function")
+    console.log("on APP Update Function");
     console.log(videoData);
     setVideoData(videoData);
     console.log(this.videoData);
@@ -35,50 +34,59 @@ function App() {
 
   useEffect(() => {
     if (videoData?.formats) {
-      setAudioFormats(videoData?.formats.filter(format => format.resolution === "audio only"));
-      setVideoFormats(videoData?.formats.filter(format => format.resolution != "audio only"));
+      setAudioFormats(
+        videoData?.formats.filter(
+          (format) => format.resolution === "audio only"
+        )
+      );
+      setVideoFormats(
+        videoData?.formats.filter((format) => format.resolution != "audio only")
+      );
     }
   }, [videoData]);
 
   useEffect(() => {
     if (videoFormatId) {
-      printConsole(videoFormatId)
+      printConsole(videoFormatId);
     }
 
-    if (audioFormatId)
-      printConsole(audioFormatId)
+    if (audioFormatId) printConsole(audioFormatId);
 
     printConsole(isDialogOpen);
-
   }, [videoFormatId, audioFormatId, isDialogOpen]);
 
-
   useEffect(() => {
-    printConsole(open)
+    printConsole(open);
 
-    printConsole("dialog Open : " + isDialogOpen)
-  }, [open, isDialogOpen])
-
+    printConsole("dialog Open : " + isDialogOpen);
+  }, [open, isDialogOpen]);
 
   const conformDownload = () => {
     setIsDialogOpen(true);
-    printConsole(isDialogOpen)
-  }
+    printConsole(isDialogOpen);
+  };
 
   return (
-    <div className='p-1'>
-
+    <div className="p-1">
       <Header setOpen={setOpen}></Header>
-      <Hero videoData={videoData} setVideoData={setVideoData} setFormatLoading={setFormatLoading} conformDownload={conformDownload}></Hero>
-      <DataTableSection videoData={videoData} formats={videoFormats} setFormatId={setVideoFormatId} formatLoading={formatLoading}></DataTableSection>
+      <Hero
+        videoData={videoData}
+        setVideoData={setVideoData}
+        setFormatLoading={setFormatLoading}
+        conformDownload={conformDownload}
+      ></Hero>
+      <DataTableSection
+        videoData={videoData}
+        formats={videoFormats}
+        setFormatId={setVideoFormatId}
+        formatLoading={formatLoading}
+      ></DataTableSection>
       <HowToUseSection> </HowToUseSection>
       <WhyToUseSection> </WhyToUseSection>
-      <Toaster position='top-right'></Toaster>
+      <Toaster position="top-right"></Toaster>
       <Footer></Footer>
-
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,92 +1,82 @@
-
-import youtube_icon from '../assets/icons/youtube.svg';
-import facebook_icon from '../assets/icons/facebook.svg'
-import instagram_icon from '../assets/icons/instagram.svg'
-import logo from '../assets/video-downloader-logo.svg';
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import youtube_icon from "../assets/icons/youtube.svg";
+import facebook_icon from "../assets/icons/facebook.svg";
+import instagram_icon from "../assets/icons/instagram.svg";
+import logo from "../assets/video-downloader-logo.svg";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 function Footer() {
-    return (
-        <footer className="flex justify-center flex-col    text-center items-center ">
+  return (
+    <footer className="flex justify-center flex-col    text-center items-center ">
+      <div className="w-11/12 bg-gradient-to-br to-blue-300 to bg-purple-300 rounded-2xl">
+        <div className="flex  flex-col lg:flex-row  border-b-2  w-full ">
+          <div className=" p-4 flex flex-col items-center lg:items-start lg:w-1/4">
+            {/* About Company Section  */}
+            <div className="mb-4 flex flex-col items-center lg:items-start">
+              <div className="flex flex-col md:flex-row items-center justify-center">
+                <img src={logo} alt="Downloader-logo " className="w-15" />
+                <p className="font-extrabold text-lg text-gray-800">
+                  DownloadMe
+                </p>
+              </div>
+              <p className="font-medium lg:text-start text-gray-500 w-3/4 ">
+                Download videos from your favorite platform quickly and easily
+              </p>
+            </div>
 
-            <div className='w-11/12 bg-gradient-to-br to-blue-300 to bg-purple-300 rounded-2xl'>
-                <div className='flex  flex-col lg:flex-row  border-b-2  w-full '>
+            <div className="flex ">
+              <Avatar className="border-2 mr-1">
+                <AvatarImage src={youtube_icon} alt="youtube logo" />
+                <AvatarFallback>YT</AvatarFallback>
+              </Avatar>
 
-                    <div className=' p-4 flex flex-col items-center lg:items-start lg:w-1/4'>
-                        {/* About Company Section  */}
-                        <div className='mb-4 flex flex-col items-center lg:items-start'>
-                            <div className='flex flex-col md:flex-row items-center justify-center'>
-                                <img src={logo} alt="Downloader-logo " className='w-15' />
-                                <p className='font-extrabold text-lg text-gray-800'>DownloadMe</p>
-                            </div>
-                            <p className='font-medium lg:text-start text-gray-500 w-3/4 '>
-                                Download videos from your favorite platform quickly and easily
-                            </p>
-                        </div>
+              <Avatar className="border-2 mr-1">
+                <AvatarImage src={facebook_icon} alt="facebook logo" />
+                <AvatarFallback>FB</AvatarFallback>
+              </Avatar>
 
-                        <div className='flex '>
-                            <Avatar className='border-2 mr-1'>
-                                <AvatarImage src={youtube_icon} />
-                                <AvatarFallback>YT</AvatarFallback>
-                            </Avatar>
+              <Avatar className="border-2 mr-1">
+                <AvatarImage src={instagram_icon} alt="instagram logo" />
+                <AvatarFallback>YT</AvatarFallback>
+              </Avatar>
+            </div>
+          </div>
 
-                            <Avatar className='border-2 mr-1'>
-                                <AvatarImage src={facebook_icon} />
-                                <AvatarFallback>FB</AvatarFallback>
-                            </Avatar>
+          <div className=" p-4 lg:w-1/4 lg:text-start lg:pt-8">
+            <h3 className="font-bold">Quick Links</h3>
+            <ul className="text-gray-500 text-md">
+              <li>Home</li>
+              <li>Download</li>
+              <li>How To Use</li>
+              <li>Features</li>
+            </ul>
+          </div>
 
-                            <Avatar className='border-2 mr-1'>
-                                <AvatarImage src={instagram_icon} />
-                                <AvatarFallback>YT</AvatarFallback>
-                            </Avatar>
-                        </div>
+          <div className=" p-4 lg:w-1/4 lg:text-start lg:pt-8">
+            <h3 className=" font-bold">Support</h3>
+            <ul className="text-gray-500">
+              <li>Privacy Policy</li>
+              <li>Terms of Service</li>
+            </ul>
+          </div>
 
+          <div className="  p-4 lg:w-1/4 lg:text-start lg:pt-8">
+            <h3>Report a bug</h3>
+          </div>
+        </div>
 
-                    </div>
+        <div className="flex flex-row justify-between  px-4 py-4 text-gray-500  w-full ">
+          <div>
+            <p>2025 DownloadMe. All rights reserved</p>
+          </div>
 
-
-                    <div className=' p-4 lg:w-1/4 lg:text-start lg:pt-8'>
-
-                        <h3 className='font-bold'>Quick Links</h3>
-                        <ul className='text-gray-500 text-md'>
-                            <li>Home</li>
-                            <li>Download</li>
-                            <li>How To Use</li>
-                            <li>Features</li>
-                        </ul>
-                    </div>
-
-
-                    <div className=' p-4 lg:w-1/4 lg:text-start lg:pt-8'>
-                        <h3 className=' font-bold'>Support</h3>
-                        <ul className='text-gray-500'>
-                            <li>Privacy Policy</li>
-                            <li>Terms of Service</li>
-                        </ul>
-                    </div>
-
-
-                    <div className='  p-4 lg:w-1/4 lg:text-start lg:pt-8'>
-                        <h3>Report a bug</h3>
-                    </div>
-
-                </div>
-
-                <div className='flex flex-row justify-between  px-4 py-4 text-gray-500  w-full '>
-                    <div>
-                        <p>2025 DownloadMe. All rights reserved</p>
-                    </div>
-
-                    {/* <div className='flex flex-row hidden '>
+          {/* <div className='flex flex-row hidden '>
     <p className='border-r-2 px-2'>Privacy</p>
     <p className='px-2'>Terms</p>
 </div> */}
-                </div>
-
-            </div>
-        </footer>
-
-    )
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
